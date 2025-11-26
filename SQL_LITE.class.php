3f88b93a00d816bb231296516b8465c94979636b
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-26 19:07:14 +0900

    Add: SQLITE > use > OP_CI

--- SQL_LITE.class.php
+++ SQL_LITE.class.php
@@ -18,6 +18,7 @@ namespace OP\UNIT\DATABASE;
  * @creation  2019-03-04
  */
 use OP\OP_CORE;
+use OP\OP_CI;
 use OP\Notice;
 
 /** SQLITE
@@ -30,6 +31,7 @@ class SQL_LITE
 	 *
 	 */
 	use OP_CORE;
+	use OP_CI;
 
 	/** Config
 	 *
