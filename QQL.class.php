2c9bddae38d8c73482ed3ec6a9bc3214a67a05e4
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-26 18:43:49 +0900

    Add: QQL > use > OP_CI

--- QQL.class.php
+++ QQL.class.php
@@ -18,6 +18,7 @@ namespace OP\UNIT\DATABASE;
  * @creation  2019-03-04
  */
 use OP\OP_CORE;
+use OP\OP_CI;
 use OP\IF_DATABASE;
 
 /**	QQL
@@ -30,6 +31,7 @@ class QQL
 	 *
 	 */
 	use OP_CORE;
+	use OP_CI;
 
 	/**	Parse option.
 	 *
