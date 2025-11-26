a18b00bb275d214e869f1ca05ff5fed8b950bd81
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-26 22:51:44 +0900

    Add: SQL_PG > use > OP_CI

--- SQL_PG.class.php
+++ SQL_PG.class.php
@@ -18,6 +18,7 @@ namespace OP\UNIT\DATABASE;
  * @creation  2019-03-04
  */
 use OP\OP_CORE;
+use OP\OP_CI;
 use OP\Notice;
 
 /**	PGSQL
@@ -30,6 +31,7 @@ class SQL_PG
 	 *
 	 */
 	use OP_CORE;
+	use OP_CI;
 
 	/**	Config
 	 *
