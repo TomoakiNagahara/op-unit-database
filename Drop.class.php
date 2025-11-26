8f7d3f2982a2270fc2b39f81e085dfa0051b6d3c
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-26 18:31:04 +0900

    Add: Drop > use > OP_CI

--- Drop.class.php
+++ Drop.class.php
@@ -18,6 +18,7 @@ namespace OP\UNIT\DATABASE;
  * @creation  2019-03-04
  */
 use OP\OP_CORE;
+use OP\OP_CI;
 use OP\IF_DATABASE;
 
 /** Drop
@@ -30,6 +31,7 @@ class Drop
 	 *
 	 */
 	use OP_CORE;
+	use OP_CI;
 
 	/** Database object.
 	 *
